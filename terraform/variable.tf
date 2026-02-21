de2ebe032a973263ed363b93c7ff0f89c9490917
saahil tanwar <saahiltanwarr@gmail.com>
2026-02-21 15:16:45 +0530

    Add GCP variables for project, region, zone, and K8s version
    Define variables for GCP project, region, zone, and Kubernetes version.

---
+++ terraform/variable.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  description = "this is gcp project-id"
+  type        = string
+  default     = "project-876aa341-49d2-4a26-aaf"
+}
+
+variable "region" {
+  description = "this is gcp region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "this is gcp zone"
+  type        = string
+  default     = "us-central1-c"
+}
+
+variable "K8s_version" {
+  description = "this is the gke version"
+  type        = string
+  default     = "1.31.6-gke.1020000"
+}
